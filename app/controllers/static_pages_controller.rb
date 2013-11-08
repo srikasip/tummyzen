f1159cb1fe6cb6d046af405112cfcd5c3d564e73
srikasip <srikasip@gmail.com>
2013-11-08 18:56:12 -0500

    Basic Layout of the site is set

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,19 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def about
+  def story
   end
+
+  def how
+  end
+
+  def purchase
+  end
+
+  def where
+  end
+
+  def faq
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Samplesite1::Application.routes.draw do
   get "static_pages/home"
-  get "static_pages/about"
+  get "static_pages/story", :as => :story
+  get "static_pages/how", :as => :how
+  get "static_pages/purchase", :as => :purchase
+  get "static_pages/faq", :as => :faq
+  get "static_pages/where", :as => :where
 
   root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
