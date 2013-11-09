8018ff051af6175e9f17f405a7092a9b32eb8835
srikasip <srikasip@gmail.com>
2013-11-09 02:39:21 -0500

    Made the basic layout of all pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,26 @@
 class StaticPagesController < ApplicationController
   def home
+    session[:curr_page] = 'home'
   end
 
   def story
+    session[:curr_page] = 'story'
   end
 
   def how
+    session[:curr_page] = 'how'
   end
 
   def purchase
+    session[:curr_page] = 'purchase'
   end
 
   def where
+    session[:curr_page] = 'where'
   end
 
   def faq
+    session[:curr_page] = 'faq'
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def page_nav
+
+    if session[:curr_page] && session[:curr_page] != ''
+      case session[:curr_page]
+      when "home"
+        page_nav = 'home'
+      when "story"
+        page_nav = 'story'
+      when "how"
+        page_nav = "how"
+      when "purchase"
+        page_nav = "purchase"
+      when "where"
+        page_nav = "purchase"
+      when "faq"
+        page_nav = "faq"
+      else
+        page_nav = "home"
+      end
+    else
+      page_nav = "home"
+    end
+    return page_nav
+  end
 end
