d5ed46c67bd8f1781e54bb1ab4693f1692b279b4
srikasip <srikasip@gmail.com>
2014-01-27 23:24:50 -0500

    Fixing Amazon Issue...take 2

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Samplesite1::Application.routes.draw do
   get "static_pages/purchase", :as => :purchase
   get "static_pages/faq", :as => :faq
   get "static_pages/where", :as => :where
+  
 
   root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
